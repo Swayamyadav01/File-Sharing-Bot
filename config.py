aa65625ae1f7113eedf8fd2ea60bf86571768a26
Swayamyadav01 <116306893+Swayamyadav01@users.noreply.github.com>
2023-08-14 15:33:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5525481199"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URl", "mongodb+srv://swayamyadav:<swayam55555>@cluster0.nmrfgh8.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URl", "mongodb+srv://swayamyadav:<swayam@55555>@cluster0.nmrfgh8.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
